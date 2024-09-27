e25d7836e3ac9543f0a9e7bdf7bd832d2fbf1e4b
PAUL RUIZ <paulruizm@gmail.com>
2024-09-26 22:50:13 -0500

    clase2

--- app02/src/app/app.routes.ts
+++ app02/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { SobreMiComponent } from './pages/sobre-mi/sobre-mi.component';
 import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { ContactanosComponent } from './pages/contactanos/contactanos.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 export const routes: Routes = [
 
   {path: 'inicio', component: InicioComponent},
   {path: 'sobremi', component: SobreMiComponent},
-  {path: 'proyectos', component: ProyectosComponent}
+  {path: 'proyectos', component: ProyectosComponent},
+  {path: 'contactanos', component: ContactanosComponent},
 
+  {path: '', redirectTo: 'inicio',pathMatch: 'full'},
+  {path: '**', component: Error404Component},
 ];
--- app02/src/app/pages/proyectos/proyectos.component.ts
+++ app02/src/app/pages/proyectos/proyectos.component.ts
@@ -9,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class ProyectosComponent {
 
+  proyectos = [
+    {
+      id: "0001",
+      tecnologia: "web",
+      lenguaje: "TypeScript",
+      tiempo_desarrollo: 8
+
+    },
+    {
+      id: "0002",
+      tecnologia: "web",
+      lenguaje: "Spring",
+      tiempo_desarrollo: 20
+    },
+    {
+      id: "0003",
+      tecnologia: "mobile",
+      lenguaje: "Kotlin",
+      tiempo_desarrollo: 15
+    },
+    {
+      id: "0004",
+      tecnologia: "mobile",
+      lenguaje: "React Native",
+      tiempo_desarrollo: 12
+    },
+    {
+      id: "0005",
+      tecnologia: "desktop",
+      lenguaje: "Java",
+      tiempo_desarrollo: 18
+    },
+    {
+      id: "0006",
+      tecnologia: "desktop",
+      lenguaje: "C#",
+      tiempo_desarrollo: 10
+    }
+  ]
 }
