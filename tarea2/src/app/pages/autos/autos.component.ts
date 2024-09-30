da4f63f687f4bd65848530d01ebdf9aac9036e44
PAUL RUIZ <paulruizm@gmail.com>
2024-09-30 16:47:19 -0500

    add lib

--- tarea2/src/app/pages/autos/autos.component.ts
+++ tarea2/src/app/pages/autos/autos.component.ts
@@ -1,11 +1,12 @@
 import { Component,  } from '@angular/core';
 import { AutosService } from '../../services/autos.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-autos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './autos.component.html',
   styleUrl: './autos.component.css'
 })
